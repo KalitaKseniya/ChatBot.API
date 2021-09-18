698081581358d557c241ece4dd73227e0172293d
Kseniya Kalita <kseniya.kalita@hb.by>
2021-09-18 22:44:24 +0300

    add permissions

---
+++ ChatBot.API/Controllers/PermissionController.cs
@@ -0,0 +1,48 @@
+﻿using ChatBot.Core.Interfaces;
+using ChatBot.Infrastructure.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatBot.API.Controllers
+{
+    [ApiController]
+    [Route("api/admin/permissions")]
+    public class PermissionController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ILoggerManager _logger;
+        private readonly UserManager<User> _userManager;
+        public PermissionController(RoleManager<IdentityRole> roleManager,
+                              ILoggerManager logger,
+                              UserManager<User> userManager)
+        {
+            _roleManager = roleManager;
+            _logger = logger;
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public IActionResult GetAllPermissions()
+        {
+
+        }
+
+        [HttpGet("{userId}")]
+        public IActionResult GetUserPermissions(string userId)
+        {
+
+        }
+
+        [HttpPut("{userId}")]
+        public IActionResult EditUserPermissions(string userId)
+        {
+
+        }
+
+        [HttpDelete("{userId}")]
+        public IActionResult DeleteUserPermission()
+        {
+
+        }
+
+    }
+}
---
+++ ChatBot.Core/Models/Permissions.cs
@@ -0,0 +1,39 @@
+﻿
+namespace ChatBot.Core.Models
+{
+    public class Permissions
+    {
+        public static class Users
+        {
+            public const string View = "users.view";
+            public const string AddRemove = "users.addremove";
+            public const string Edit = "users.edit";
+            public const string EditRoles = "users.edit.roles";
+            public const string ViewRoles = "users.view.roles";
+            public const string ChangePassword = "users.changepassword";
+        }
+
+        public static class Chats
+        {
+            public const string Add = "chats.add";
+            public const string Edit = "chats.edit";
+            public const string ViewById = "chats.viewById";
+            public const string Delete = "chats.delete";
+        }
+
+        public static class Claims
+        {
+            public const string View = "claims.view";
+        }
+
+        public static class Roles
+        {
+            public const string View = "roles.view";
+            public const string AddRemove = "roles.addremove";
+            public const string ViewClaims = "roles.view.claims";
+            public const string EditClaims = "roles.edit.claims";
+        }
+
+
+    }
+}
